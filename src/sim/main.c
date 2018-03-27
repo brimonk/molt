cc6a48f5a0cf2594a71035615a7d4b50a7346648
Brian Chrzanowski <chrzanowskitest@gmail.com>
2018-03-27 00:11:41 -0400

    added basic main for the simulation project

---
+++ src/sim/main.c
@@ -0,0 +1,16 @@
+/*
+ * Brian Chrzanowski
+ * Tue Mar 27, 2018 00:09
+ *
+ * main of the simulator
+ */
+
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
