55c8f44acd2c0fa96d651563e6e1c2455bd67645
brimonk <bflatmaster@gmail.com>
2018-07-27 13:53:28 -0400

    Revert "Merge branch 'particle-cleanup' of chrzanowski.me:~/repos/molt"
    This reverts commit 8da513944fb087f8874a7729a2bd9d8669731c69, reversing
    changes made to b1bc08aa2293e3fe765f5a821a80fb28c41f101a.

--- src/main.c
+++ src/main.c
@@ -1,22 +1,10 @@
-/*
- * Brian Chrzanowski
- * Fri Jul 20, 2018 13:46
- *
- * Extended from
- *		Mathematical work done by Matthew Causley
- *		Particle simulation skeletoned by Khari Gray
- */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-
-#include "sqlite3.h"
-#include "io.h"
+//  Force on a point charge
+//
+//  Created by Khari Gray on 4/16/18.
+//  Copyright © 2018 Khari Gray. All rights reserved.
 
 #include "structures.h"
-#include "particles.h"
 
 // necessary prototypes
 void updatePosition(int, int);
@@ -26,54 +14,37 @@ struct vector vectorize(float, float, float);
 float Fx(float, float, float, float, float, float, float, float, float);
 float Fy(float, float, float, float, float, float, float, float, float);
 float Fz(float, float, float, float, float, float, float, float, float);
-int molt_run(sqlite3 *db);
 
 // declaring global variables
 float timeInitial, timeFinal, timeChange, currTime;
 struct vector s[999][999], v[999][999], E, B; // position, velocity, electric field, magnetic field
 
+#include <time.h> // innate C libraries
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 // user defined libraries
 #include "files.h" // funcitons that relate to writing all the data to file to be read and plotted by Matlab
 #include "displayData.h" // functions the display the data to the user in the console
 #include "eulersMethod.h" // functions that perform iterative techniques on position and velocity
+#include "miscellaneous.h" // simple functions, doesn't affect the project's idea
 #include "changingFields.h" // changing field
 #include "forceCalculation.h" // functinos to calculate the force
 #include "initialParameters.h" // functions that accept the intiial condition for the simulation
 
-#define DATABASE "molt_output.db"
-
-int main(int argc, char **argv)
+int main()
 {
-    int val;
-	sqlite3 *db;
+    int parNo, noPar, loopCount, timeInd = 0; // timeInd indicates the iteration in time that the simulation is at
     
-	/* set up the database */
-	val = sqlite3_open(DATABASE, &db);
-
-	if (val != SQLITE_OK) {
-		SQLITE3_ERR(val);
-	}
-
-	process_sql_tbls(db, io_db_tbls);
-
-	molt_run(db);
-
-	sqlite3_close(db);
-
-	return 0;
-}
-
-int molt_run(sqlite3 *db)
-{
-    int val, parNo, noPar, loopCount, timeInd = 0; // timeInd indicates the iteration in time that the simulation is at
-
     noPar = initialParameters(); // reading the initial parameters
     parNo = 1;
-
+    tableHeading(); // printing the heading for the table (position .. velocity)
     displayInitialParameters(parNo, noPar, timeInd);  // displaying the initial parameters for all the particles
     
     while(currTime < timeFinal) // plotting the position for each particle at each time step
     {
+        printNewLine();
         for(parNo=1; parNo<(noPar+1); parNo++) // repeating funtions per number of particles
         {
             
@@ -88,12 +59,10 @@ int molt_run(sqlite3 *db)
         }
         currTime = currTime + timeChange; // incrementing time value
     }
-
     loopCount = timeInd;
     
     fileManipulation(timeInd, loopCount, parNo, noPar); // writes all the data to .csv files, to be later read and plotted by Matlab
-
-	printf("\n");
-
-	return val;
+    closingStatement(); // printing an exit statement for the user
+    
+    return 0;
 }
---
+++ src/miscellaneous.h
@@ -0,0 +1,15 @@
+
+void printNewLine()
+{
+    printf("\n");
+}
+
+void tableHeading()
+{
+    printf("\n\t\t\tposition\t\t\t\t  velocity\n"); // heading for the table
+}
+
+void closingStatement()
+{
+    printf("\n\nThank you for using this program.\n\n"); // closing remarks
+}
