1469c1e6211bf6b630f969ea5e61e3b26f9885d1
brimonk <bflatmaster@gmail.com>
2018-07-27 15:02:53 -0400

    removed unneeded printing to stdout routines (data structures changing)

--- src/main.c
+++ src/main.c
@@ -22,7 +22,6 @@
 // necessary prototypes
 void updatePosition(int, int);
 void updateVelocity(int, int);
-void displayInitialParameters(int, int, int);
 struct vector vectorize(float, float, float);
 float Fx(float, float, float, float, float, float, float, float, float);
 float Fy(float, float, float, float, float, float, float, float, float);
@@ -35,7 +34,6 @@ struct vector s[999][999], v[999][999], E, B; // position, velocity, electric fi
 
 // user defined libraries
 #include "files.h" // funcitons that relate to writing all the data to file to be read and plotted by Matlab
-#include "displayData.h" // functions the display the data to the user in the console
 #include "eulersMethod.h" // functions that perform iterative techniques on position and velocity
 #include "changingFields.h" // changing field
 #include "forceCalculation.h" // functinos to calculate the force
@@ -75,8 +73,6 @@ int molt_run(sqlite3 *db)
     noPar = initialParameters(); // reading the initial parameters
     parNo = 1;
 
-    displayInitialParameters(parNo, noPar, timeInd);  // displaying the initial parameters for all the particles
-    
     while(currTime < timeFinal) // plotting the position for each particle at each time step
     {
         for(parNo=1; parNo<(noPar+1); parNo++) // repeating funtions per number of particles
@@ -85,7 +81,6 @@ int molt_run(sqlite3 *db)
             changingFields(parNo, timeInd); // dynamizing the fields
             updatePosition(parNo, timeInd); // updating position values
             updateVelocity(parNo, timeInd); // updating velocity values
-            displayUpdatedData(parNo, noPar, timeInd); // displaying the updated position and velocity data
             if(parNo == noPar) // iterates to the next time step when all data has been collected for all particles for that instance
             {
                 timeInd++;
@@ -99,7 +94,7 @@ int molt_run(sqlite3 *db)
     // fileManipulation(timeInd, loopCount, parNo, noPar); // writes all the data to .csv files, to be later read and plotted by Matlab
 
 	/* dump the output into SQLite */
-	io_insert(db, io_particle_insert, &s);
+	// io_insert(db, io_particle_insert, NULL);
 
 	printf("\n");
 
