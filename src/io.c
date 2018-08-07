5057bb1b8dae7b196aacdb1c88b81f69b01d2c78
Brian Chrzanowski <chrzanowskitest@gmail.com>
2018-08-07 04:56:44 -0400

    working on some of these late-night IO system ideas:
    * Wrapper structure to make argument passing clean
    * Function in each wrapper must be designed as a callback to each data item,
      plopping each element of the structure into a buffer of items_in_struct
      size
    * this callback function will (probably) be called for each item in the queue

--- src/io.c
+++ src/io.c
@@ -20,6 +20,11 @@
 #include "io.h"
 #include "particles.h"
 
+static int io_dbwrap_select();
+static int io_dbwrap_insert();
+static int io_dbwrap_update();
+static int io_dbwrap_delete();
+
 /* this should be gathered dynamically from some rules somewhere */
 char *io_db_tbls[] = 
 {
@@ -68,14 +73,94 @@ struct err_tbl error_lookups[] = {
 };
 
 /*
- * As of now, because the program only has to INSERT into SQLite (all other
- * work is then done in	SQL), I've decided it's probably better for a single,
- * specific routine for each core piece of the application
+ * The current plan, because it'll be really easy to extend later in time,
+ * is to write generic-ish functions for select, insert, update, and delete.
  *
- * Fri Jul 27, 2018 14:07
+ * Each function will take a sqlite3 handle, a sql stmt to be run, a pointer
+ * to an array of data, and a pointer to a function to execute to get
+ * the raw form of that data.
+ *
+ * It's not the _best_, but shy of writing an ORM in a language that doesn't
+ * have ORMs, it'll be fine.
+ *
+ * Another option is to wrap everything in a structure that looks something
+ * like this
+ *
+ * struct db_wrap_t {
+ * 		int debug; // optional
+ * 		int op; // select = 1, insert = 2, update = 4, del = 8, other = err
+ * 		char *sql;
+ * 		void *data;
+ * 		int datasize;
+ * 		int (func *)(void *data, char **bufs)
+ * };
+ *
+ * Where a pointer to this can then be passed to a single function, and
+ * logic will flow to specific functions to do what I discussed above.
  */
 
-int io_insert(sqlite3 *db, char *sql, struct particle_t *parts)
+enum {
+	IO_DBWRAP_SELECT = 0x01,
+	IO_DBWRAP_INSERT = 0x02,
+	IO_DBWRAP_UPDATE = 0x04,
+	IO_DBWRAP_DELETE = 0x08
+};
+
+struct db_wrap_t {
+	int op;
+	int struct_size;
+	int items_in_struct;
+	int items;
+	char *sql;
+	void *data;
+	int (*func)(void *ptr, char **bufs);
+};
+
+int io_dbwrap_do(struct db_wrap_t *wrapper)
+{
+	int val;
+	val = 0;
+
+	switch (wrapper->op) {
+	case IO_DBWRAP_SELECT:
+		io_dbwrap_select();
+		break;
+
+	case IO_DBWRAP_INSERT:
+		io_dbwrap_insert();
+		break;
+
+	case IO_DBWRAP_UPDATE:
+		io_dbwrap_update();
+		break;
+
+	case IO_DBWRAP_DELETE:
+		io_dbwrap_delete();
+		break;
+
+	default:
+		break;
+	}
+
+	return val;
+}
+
+static int io_dbwrap_select()
+{
+	return 0;
+}
+
+static int io_dbwrap_insert()
+{
+	return 0;
+}
+
+static int io_dbwrap_update()
+{
+	return 0;
+}
+
+static int io_dbwrap_delete()
 {
 	return 0;
 }
