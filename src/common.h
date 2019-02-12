9f967c92198e3a48dd9f0488e6e3e9a68f41ad63
Brian Chrzanowski <chrzanowskitest@gmail.com>
2019-02-12 04:01:18 -0500

    added index helpers, for when I go and begin converting the "hard" work

--- src/common.h
+++ src/common.h
@@ -1,6 +1,10 @@
 #ifndef MOLT_COMMON
 #define MOLT_COMMON
 
+/* macros to help index into our 2d and 3d arrays */
+#define IDX_2D(x, y, len)    ((x) * (len) + (y))
+#define IDX_3D(x, y, z, len) (((x) * (len) * (len)) + ((y) * (len)) + (z))
+
 void print_err_and_die(char *msg, char *file, int line);
 void swapd(double *a, double *b);
 
